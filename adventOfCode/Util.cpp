5b7b72dee4e8ceca2b75e40b54c72ef298aa4059
Reinout Versteeg <rcw.versteeg@avans.nl>
2017-12-01 19:56:21 +0100

    remove comments

--- adventOfCode/Util.cpp
+++ adventOfCode/Util.cpp
@@ -8,14 +8,10 @@ bool utility::readFileToBuffer(std::string filePath, std::vector<unsigned char>&
 		return false;
 	}
 
-	//seek to the end
 	file.seekg(0, std::ios::end);
 
-	//Get the file size
 	int fileSize = file.tellg();
 	file.seekg(0, std::ios::beg);
-
-	//Reduce the file size by any header bytes that might be present
 	fileSize -= file.tellg();
 
 	buffer.resize(fileSize);
