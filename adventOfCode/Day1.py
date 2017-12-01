27276967888d4808f9a865fa67474536fc007125
Reinout Versteeg <rcw.versteeg@avans.nl>
2017-12-01 17:03:18 +0100

    python solution day 1 and source file

---
+++ adventOfCode/Day1.py
@@ -0,0 +1,18 @@
+import os
+
+def Input():
+	"Open input file of corresponding day."
+	print os.path.splitext(os.path.basename(__file__))[0]
+	with open('{}.txt'.format(os.path.splitext(os.path.basename(__file__))[0]), 'r') as myfile:
+		data=myfile.read()
+	return data
+
+#Day 1: Inverse Captcha
+input = Input()
+total = 0
+shift = len(input)/2 # for first puzzle, shift = 1
+for idx, c in enumerate(input):
+	if int(c) == int(input[(idx+shift)%len(input)]):
+		total = total + int(c)
+
+print total
