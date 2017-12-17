7a103fb9da0d7db0d097ea61e7461bab739cacbc
reynouts <rcw.versteeg@avans.nl>
2017-12-17 09:47:59 +0100

    bug fix

--- adventOfCode/Python/Day17.py
+++ adventOfCode/Python/Day17.py
@@ -9,16 +9,16 @@ for i in xrange(2018):
 	currentPosition = (currentPosition + skipInput + 1) % len(myList)
 
 index = myList.index(2017)
-print "Part 1 /// the element after 2017 is: {}").format(myList[index+1])
+print "Part 1 /// the element after 2017 is: {}".format(myList[index+1])
 
 length = 0
 lastElement = 0
 currentPosition = 0
 
 for i in xrange(50000000):
-	if currentPosition == 0:
+	if currentPosition == 1:
 		lastElement = i
 	length += 1
-	currentPosition = (currentPosition + skipInput + 1) % length
+	currentPosition = ((currentPosition + skipInput) % length)+1
 
-print "Part 1 /// the element after 0 is: {}").format(lastElement)
+print "Part 2 /// the element after 0 is: {}".format(lastElement)
